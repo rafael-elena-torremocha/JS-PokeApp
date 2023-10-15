d584ecb1462052a7fde97ffa39a306ebc1b84a56
rafael-elena-torremocha <torremocha.rafael@gmail.com>
2023-10-15 17:10:32 +0200

    firstCommit

---
+++ src/app/index.js
@@ -0,0 +1,11 @@
+import './styles/styles.scss';
+import 'bootstrap';
+
+function addListeners() {
+  // TODO
+  console.log('addListeners');
+}
+
+window.onload = function () {
+  addListeners();
+};
